4bf77b37db212d2af447d690e58a670e0c1b2cc6
Esmeralda Anaya Mauri <nanani170823@gmail.com>
2025-06-15 19:23:09 -0300

    Update upload tests to include new scenarios for file upload via button and drag-and-drop, enhancing test coverage and clarity.

--- cypress/e2e/upload.cy.ts
+++ cypress/e2e/upload.cy.ts
@@ -5,7 +5,7 @@ describe('Upload Test', () => {
     cy.visit('https://the-internet.herokuapp.com/upload')
   })
 
-  it('should upload file successfully', () => {
+  it('sFazer upload através do botão ‘choose file’', () => {
     // Usar una ruta relativa desde la raíz del proyecto
     cy.get('#file-upload').selectFile('cypress/upload/file1.pdf')
     
@@ -17,5 +17,16 @@ describe('Upload Test', () => {
     
     // Verificar que el upload fue exitoso
     cy.get('h3').should('contain', 'File Uploaded!')
+  })
+
+  it('Fazer upload através da área de upload marcada', () => {
+
+   // Hacer click en el botón de upload
+    cy.get('#drag-drop-upload').click()
+    
+  })
+
+  it('Fazer upload de uma sequência de arquivos', () => {
+    
   })
 })
